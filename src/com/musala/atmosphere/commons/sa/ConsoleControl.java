73610ba109e9d3beb1bb02563d4711ad239d8a8e
vladimir.vladimirov <vladimir.vladimirov@34708651-a5da-c34d-b20c-10ddab38317d>
2014-02-17 16:03:31 +0000

    Initial import:
     * New state on the agent - DisconnectedAgent;
     * Several Unit tests for the following commands: server-address, performance, devices and one Base unit test which all other tests inherit.
    
    Changeset:
    
     * Completely refactored the N. Taushanov state design patter;
     * The result is:
      * 17 removed classes - mostly commands, states of the agents and tests;
      * Refactored the whole command factory;
      * Changed the logic flow when executing a command from the agent;
      * Reduced a lot of duplicated and unnecessary code;
      * Removed the problematic thread that just won't stop working when executing 'exit' command;
     * Added some log messages where I thought it was necessary to refactor the old code;
     * Changed the ConsoleControl to separate string commands to <String, List<String>> instead of <String, String[]> and refactored the Server code a little to keep the things compatible and compilable;
    
    Notes:
    
     * Fixed the bug with the inner thread in the ( none-existing anymore ) RunningState of the agent.
    
    [http://jenkins.musala.com/r/6835/ $6835] see #2730
    
    git-svn-id: https://trac.musala.com/ARDC/repo@9674 34708651-a5da-c34d-b20c-10ddab38317d

--- src/com/musala/atmosphere/commons/sa/ConsoleControl.java
+++ src/com/musala/atmosphere/commons/sa/ConsoleControl.java
@@ -5,114 +5,107 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 
 import com.musala.atmosphere.commons.util.Pair;
 
-public class ConsoleControl
-{
-	private static final String LINE_SEPARATOR = System.getProperty("line.separator");
+public class ConsoleControl {
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
 
-	private static final String LINE_PREFFIX = ">> ";
+    private static final String LINE_PREFFIX = ">> ";
 
-	private static final Logger LOGGER = Logger.getLogger(ConsoleControl.class.getCanonicalName());
+    private static final Logger LOGGER = Logger.getLogger(ConsoleControl.class.getCanonicalName());
 
-	private BufferedReader consoleReader;
+    private BufferedReader consoleReader;
 
-	private BufferedWriter consoleWriter;
+    private BufferedWriter consoleWriter;
 
-	/**
-	 * Creates console for the agent. If Agent code is ran through some IDE ( for example Eclipse ) we must use
-	 * <b>System.in</b> and <b>System.out</b> to manage the agent using text commands ( because System.console() is null
-	 * when JVM is started automatically, for example - when it's launched from an IDE ); if the agent is ran through
-	 * command prompt, then System.console() should be used.
-	 */
-	public ConsoleControl()
-	{
-		consoleReader = new BufferedReader(new InputStreamReader(System.in));
-		consoleWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-	}
+    /**
+     * Creates console for the agent and associates it with {@link System#in System.in} and {@link System#out
+     * System.out}.
+     */
+    public ConsoleControl() {
+        consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        consoleWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
 
-	/**
-	 * Prints command prompt message and read the rest of the line.
-	 * 
-	 * @return - A String containing the command and parameters of the user.
-	 * @throws IOException
-	 */
-	public String readCommand() throws IOException
-	{
-		write(LINE_PREFFIX);
-		String line = readLine();
-		return line;
-	}
+    /**
+     * Prints command prompt message and read the rest of the line.
+     * 
+     * @return - A String containing the command and parameters of the user.
+     * @throws IOException
+     */
+    public String readCommand() throws IOException {
+        write(LINE_PREFFIX);
+        String line = readLine();
+        return line;
+    }
 
-	/**
-	 * Reads single line of text from the console. A line is considered to be terminated by any one of a line feed
-	 * ('\n'), a carriage return ('\r'), or a carriage return followed immediately by a linefeed.
-	 * 
-	 * @return - A String containing the contents of the line, not including any line-termination characters, or null if
-	 *         the end of the stream has been reached
-	 * @throws IOException
-	 *         - if the agent is started through IDE, it is possible for an IOException to be thrown while reading from
-	 *         the System.in
-	 */
-	public String readLine() throws IOException
-	{
-		String line = consoleReader.readLine();
-		return line;
-	}
+    /**
+     * Reads single line of text from the console. A line is considered to be terminated by any one of a line feed
+     * ('\n'), a carriage return ('\r'), or a carriage return followed immediately by a linefeed.
+     * 
+     * @return - A String containing the contents of the line, not including any line-termination characters, or null if
+     *         the end of the stream has been reached
+     * @throws IOException
+     *         - if the agent is started through IDE, it is possible for an IOException to be thrown while reading from
+     *         the System.in
+     */
+    public String readLine() throws IOException {
+        String line = consoleReader.readLine();
+        return line;
+    }
 
-	/**
-	 * Prints message to the agent console. No new line will be added to the end of the message.
-	 * 
-	 * @param message
-	 *        - message or command to be written on the agent console.
-	 */
-	public void write(String message)
-	{
-		try
-		{
-			consoleWriter.write(message);
-			consoleWriter.flush();
-		}
-		catch (IOException e)
-		{
-			LOGGER.error("Error while printing information on the console.", e);
-		}
-	}
+    /**
+     * Prints message to the agent console. No new line will be added to the end of the message.
+     * 
+     * @param message
+     *        - message or command to be written on the agent console.
+     */
+    public void write(String message) {
+        try {
+            consoleWriter.write(message);
+            consoleWriter.flush();
+        }
+        catch (IOException e) {
+            LOGGER.error("Error while printing information on the console.", e);
+        }
+    }
 
-	/**
-	 * Prints line of text to the console. The line consists of the given text, concatenated with the character for new
-	 * line.
-	 * 
-	 * @param message
-	 *        - message or command to be written on the agent console.
-	 */
-	public void writeLine(String message)
-	{
-		write(message + LINE_SEPARATOR);
-	}
+    /**
+     * Prints line of text to the console. The line consists of the given text, concatenated with the character for new
+     * line.
+     * 
+     * @param message
+     *        - message or command to be written on the agent console.
+     */
+    public void writeLine(String message) {
+        write(message + LINE_SEPARATOR);
+    }
 
-	/**
-	 * Parses given shell command in two: command and parameters, and returns the result as a Pair.
-	 * 
-	 * @param passedCommand
-	 * @return - Pair<String, String[]> where the key is the command and the value is array with the passed parameters
-	 *         as strings.
-	 */
-	public static Pair<String, String[]> parseShellCommand(String passedCommand)
-	{
-		// parsing the command where character is ' ' OR ':'
-		String[] args = passedCommand.trim().split("[ :]");
-		int numberOfParams = args.length - 1;
-		String command = args[0];
-		String[] params = new String[numberOfParams];
+    /**
+     * Parses given shell command in two: command and parameters, and returns the result as a Pair.
+     * 
+     * @param passedCommand
+     *        - full command for execution.
+     * @return Pair<String, List<String> > where the key is the command and the value is a list with the passed
+     *         parameters as strings.
+     */
+    public static Pair<String, List<String>> parseShellCommand(String passedCommand) {
+        // parsing the command where character is ' ' OR ':'
+        String[] args = passedCommand.trim().split("[ :]");
+        int numberOfParams = args.length - 1;
+        String command = args[0];
+        String[] params = new String[numberOfParams];
 
-		// Copy args array in params shifted with one position.
-		System.arraycopy(args, 1, params, 0, numberOfParams);
+        // Copy args array in params shifted with one position.
+        System.arraycopy(args, 1, params, 0, numberOfParams);
 
-		Pair<String, String[]> parsedShellCommand = new Pair<String, String[]>(command, params);
-		return parsedShellCommand;
-	}
+        Pair<String, List<String>> parsedShellCommand = new Pair<String, List<String>>(command, Arrays.asList(params));
+        return parsedShellCommand;
+    }
+
 }
