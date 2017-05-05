9ff636c4fe44bf174509a4cad84d9fa454169254
Dimcho N. Nedev <dimcho.nedev@musala.com>
2017-05-05 20:12:04 +0300

    Remove the redundant serialVersionUIDs and еmpty or auto generated comments
    The commit is a part of the WebSocket migration

--- src/main/java/com/musala/atmosphere/commons/sa/exceptions/DeviceNotFoundException.java
+++ src/main/java/com/musala/atmosphere/commons/sa/exceptions/DeviceNotFoundException.java
@@ -2,14 +2,11 @@ package com.musala.atmosphere.commons.sa.exceptions;
 
 /**
  * Thrown when a method an Agent method was invoked with serial number of a device that is not present on the Agent.
- * 
+ *
  * @author georgi.gaydarov
- * 
+ *
  */
 public class DeviceNotFoundException extends Exception {
-    /**
-	 * 
-	 */
     private static final long serialVersionUID = -2912674159916966023L;
 
     public DeviceNotFoundException() {
--- src/main/java/com/musala/atmosphere/commons/sa/exceptions/NotPossibleForDeviceException.java
+++ src/main/java/com/musala/atmosphere/commons/sa/exceptions/NotPossibleForDeviceException.java
@@ -2,14 +2,11 @@ package com.musala.atmosphere.commons.sa.exceptions;
 
 /**
  * Thrown when a command for real devices only was attempted on an emulator and vice versa.
- * 
+ *
  * @author georgi.gaydarov
- * 
+ *
  */
 public class NotPossibleForDeviceException extends Exception {
-    /**
-	 * 
-	 */
     private static final long serialVersionUID = -5770462758201830157L;
 
     public NotPossibleForDeviceException() {
